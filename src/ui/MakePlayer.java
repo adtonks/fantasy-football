9dec008864d4109d0e533cce4bed034a5f649103
charisannelim <chariswork@gmail.com>
2017-11-26 23:11:00 +0800

    Took out "" from img_path

--- src/ui/MakePlayer.java
+++ src/ui/MakePlayer.java
@@ -10,14 +10,20 @@ import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.imageio.ImageIO;
 
 import listeners.SnaptoGridListener;
 
 public class MakePlayer extends JPanel {
-
 	
-	public MakePlayer(Font textFont, ImageIcon image, String name, String position) {
-			
+	int playerID;
+
+ public MakePlayer(Font textFont, String path, String name, String position, int playerID) {
+	 
+	 		this.playerID = playerID;
+
+			ImageIcon image = new ImageIcon(path);		
+		
 			this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 			this.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 			this.setBackground(new Color(0, 0, 0, 0));
@@ -50,37 +56,9 @@ public class MakePlayer extends JPanel {
 			this.add(Box.createRigidArea(new Dimension(0, 3)));
 			this.add(position_);
 		}
+ 
+ public int getID() {
+	 return playerID;
+ }
 		
-//	public MakePlayer(Font textFont, ImageIcon image, String name, String position) {
-//	
-//	this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-//	this.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-//	this.setBackground(new Color(104, 0, 0));
-//	
-//	Font newFont = textFont.deriveFont((float) 12);
-//	
-//	//Picture
-//	JLabel pic = new JLabel("", image, JLabel.CENTER);
-//	pic.setAlignmentX(CENTER_ALIGNMENT);
-//	
-//	//Name and Position
-//	JLabel name_ = new JLabel(name);
-//	name_.setFont(newFont);
-//	name_.setAlignmentX(CENTER_ALIGNMENT);
-//	name_.setBackground(new Color(104, 0, 0));
-//	name_.setForeground(Color.white);
-//	
-//	JLabel position_ = new JLabel(position);
-//	position_.setFont(newFont);
-//	position_.setAlignmentX(CENTER_ALIGNMENT);
-//	position_.setBackground(new Color(104, 0, 0));
-//	position_.setForeground(Color.white);
-//	
-//	this.add(pic);
-//	this.add(Box.createRigidArea(new Dimension(0, 3)));
-//	this.add(name_);
-//	this.add(Box.createRigidArea(new Dimension(0, 3)));
-//	this.add(position_);
-//}
-//
 }
